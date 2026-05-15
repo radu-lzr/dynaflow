94dc0252eedd9024990f973d06e7591cee770443
Radu Lazar <radulucianlazar@icloud.com>
2026-05-15 22:52:35 +0200

    update core package with postgres plugin

---
+++ dynolab/packages/core/src/postgres.ts
@@ -0,0 +1,26 @@
+import pg from 'pg';
+import fp from 'fastify-plugin';
+import type { FastifyInstance } from 'fastify';
+
+declare module 'fastify' {
+    export interface FastifyInstance {
+        pg: pg.Pool;
+    }
+};
+export default fp(async (fastify: FastifyInstance) => {
+    const pool = new pg.Pool({
+        host: process.env.POSTGRES_HOST,
+        port: parseInt(process.env.POSTGRES_PORT || '5432'),
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+    });
+
+    await pool.query(`SELECT 1`); // Test connection
+
+    fastify.decorate('pg', pool);
+    fastify.addHook('onClose', async () => {
+        await pool.end();
+    });
+});
+
