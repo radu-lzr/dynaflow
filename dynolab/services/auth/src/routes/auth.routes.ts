071d72c95fd843862d30cc7f34366712dbe20785
Radu Lazar <radulucianlazar@icloud.com>
2026-05-18 18:41:12 +0200

    routes for auth

---
+++ dynolab/services/auth/src/routes/auth.routes.ts
@@ -0,0 +1,10 @@
+import type { FastifyInstance } from 'fastify';
+import { RegisterUserSchema, LoginUserSchema, RefreshTokenSchema, LogoutUserSchema } from '../schemas/auth.schema';
+import { registerUserHandler, loginUserHandler, refreshTokenHandler, logoutUserHandler } from '../handlers/auth.handlers';
+
+export async function authRoutes(fastify: FastifyInstance) {
+    fastify.post('/auth/register', { schema: RegisterUserSchema }, registerUserHandler);
+    fastify.post('/auth/login',    { schema: LoginUserSchema },    loginUserHandler);
+    fastify.post('/auth/refresh',  { schema: RefreshTokenSchema }, refreshTokenHandler);
+    fastify.post('/auth/logout',   { schema: LogoutUserSchema },   logoutUserHandler);
+}
